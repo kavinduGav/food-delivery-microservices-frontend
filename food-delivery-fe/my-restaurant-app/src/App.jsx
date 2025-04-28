88c94654660111c2d298e9ec81f763787f34b78a
Shehan Salitha <152364600+shehan8461@users.noreply.github.com>
2025-04-28 21:54:49 +0530

    update app .jsx

--- food-delivery-fe/my-restaurant-app/src/App.jsx
+++ food-delivery-fe/my-restaurant-app/src/App.jsx
@@ -4,10 +4,6 @@ import SignupPage from './pages/auth/SignupPage';
 import Restaurants from './pages/customer/Restaurants';
 import RestaurantDashboard from './pages/restaurant/RestaurantDashboard';
 import AdminDashboard from './pages/admin/AdminDashboard';
-import SubmitFeedback from './microservices/feedback/SubmitFeedback';
-// import SignIn from './microservices/feedback/Signin';
-// import SignUp from './microservices/feedback/SignUp';
-import Profile from './microservices/feedback/Profile';
 
 function App() {
   return (
@@ -19,14 +15,6 @@ function App() {
         <Route path="/restaurant-dashboard" element={<RestaurantDashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/" element={<div>Home Page</div>} />
-
-
-
-        <Route path="/SubmitFeedback" element={<SubmitFeedback />} />
-        {/* <Route path="/signinNew" element={<SignIn />} />
-        <Route path="/SignUPnEW" element={<SignUp />} /> */}
-        <Route path="/profile" element={<Profile />} />
-
       </Routes>
     </Router>
   );
