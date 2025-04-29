11b4236ed1e001bb20135cb78ccb8d67ff94608c
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2025-04-29 08:41:26 +0530

    Update App.jsx

--- food-delivery-fe/my-restaurant-app/src/App.jsx
+++ food-delivery-fe/my-restaurant-app/src/App.jsx
@@ -12,6 +12,14 @@ import FeedbackProfile from './microservices/feedback/FeedbackProfile';
 import AddDiliverRole from './microservices/DiliverRole/CreateDiliverRole';
 import DiliveryDetailsProfile from './microservices/DiliverRole/DiliveryDetailsProfile';
 import CompleteDiliveryDetails from './microservices/DiliverRole/CompleteDiliveryDetails';
+import CheckoutPage from './microservices/payment/CheckoutPage';
+import CreditCardForm from './microservices/payment/CreditCardForm';
+import PayHereForm from './microservices/payment/PayHereForm';
+import PaymentConfirmationPage from './microservices/payment/PaymentConfirmationPage';
+import PaymentDetailPage from './microservices/payment/PaymentDetailPage';
+import PaymentHistoryPage from './microservices/payment/PaymentHistoryPage';
+import PaymentMethod from './microservices/payment/PaymentMethod';
+import PaymentSummary from './microservices/payment/PaymentSummary';
 
 function App() {
   return (
@@ -35,6 +43,19 @@ function App() {
         <Route path="/AddDiliverRole" element={<AddDiliverRole />} />
         <Route path="/DiliveryDetailsProfile" element={<DiliveryDetailsProfile />} />
         <Route path="/update-task/:id" element={<CompleteDiliveryDetails />} />
+
+
+        <Route path="/checkout/:orderId" element={<CheckoutPage />} />
+        {/* <Route path="/CreditCardForm" element={<CreditCardForm />} />
+        <Route path="/PayHereForm" element={<PayHereForm />} /> */}
+        <Route path="/payment/success" element={<PaymentConfirmationPage success={true} />} />
+        <Route path="/payment/cancel" element={<PaymentConfirmationPage success={false} />} />
+        <Route path="/payment/:paymentId"  element={<PaymentDetailPage/>} />
+        <Route path="/payment/history" element={<PaymentHistoryPage/>} />
+        <Route path="/PaymentMethod" element={<PaymentMethod/>} />
+        <Route path="/PaymentSummary" element={<PaymentSummary/>} />
+        {/* Default route */}
+        <Route path="/checkoutPage" element={<CheckoutPage />} />
 
       </Routes>
     </Router>
